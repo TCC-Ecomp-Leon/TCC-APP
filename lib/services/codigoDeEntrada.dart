8f218f3531e5d713cc0a12b9e8f0f98b9c106a61
LeonLTS <leon_lts@hotmail.com>
2022-01-08 19:32:39 -0300

    feat: implementação dos services relacionados ao código de entrada

---
+++ lib/services/codigoDeEntrada.dart
@@ -0,0 +1,74 @@
+import 'package:dio/dio.dart';
+import 'package:tcc_app/models/CodigoEntrada.dart';
+import 'package:tcc_app/services/Endpoints.dart';
+
+import './requests/dio.dart';
+
+Future<List<CodigoEntrada>?> obterCodigosDeEntrada() {
+  return executeRequest(
+    () {
+      return httpClient.request(
+        Endpoints.codigoDeEntradaEndpoint,
+        options: Options(method: 'GET'),
+      );
+    },
+    (Response<dynamic> response) {
+      final body = response.data as Map<String, dynamic>;
+      return (body['codigos'] as List<dynamic>)
+          .map((json) => CodigoEntrada.fromJson(json))
+          .toList();
+    },
+  );
+}
+
+Future<CodigoEntrada?> registrarCodigoDeEntrada(
+  TipoCodigoDeEntrada tipo,
+  String idCurso,
+  String? idMateria,
+) {
+  return executeRequest(
+    () {
+      return httpClient.request(
+        Endpoints.codigoDeEntradaEndpoint,
+        data: {
+          'tipo': tipo,
+          'idCurso': idCurso,
+          'idMateria': idMateria,
+        },
+        options: Options(method: 'POST'),
+      );
+    },
+    (Response<dynamic> response) {
+      final body = response.data as Map<String, dynamic>;
+      return CodigoEntrada.fromJson(body['codigo']);
+    },
+  );
+}
+
+Future<bool?> usarCodigoDeEntrada(String idCodigo) {
+  return executeRequest(
+    () {
+      return httpClient.request(
+        Endpoints.codigoDeEntradaEndpoint + "/" + idCodigo,
+        options: Options(method: 'PUT'),
+      );
+    },
+    (Response<dynamic> response) {
+      return true;
+    },
+  );
+}
+
+Future<bool?> removerCodigoDeEntrada(String idCodigo) {
+  return executeRequest(
+    () {
+      return httpClient.request(
+        Endpoints.codigoDeEntradaEndpoint + "/" + idCodigo,
+        options: Options(method: 'DELETE'),
+      );
+    },
+    (Response<dynamic> response) {
+      return true;
+    },
+  );
+}
