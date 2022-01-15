8b0e3055f570822d1fc18ef4f17231a5517dd2a4
LeonLTS <leon_lts@hotmail.com>
2022-01-15 09:37:50 -0300

    refactor: criação de widgets para os cards do header da página de cursos

--- lib/app/modules/cursos/modules/visualizacaoCurso/visualizacao_curso_view.dart
+++ lib/app/modules/cursos/modules/visualizacaoCurso/visualizacao_curso_view.dart
@@ -1,13 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:tcc_app/app/modules/cursos/modules/visualizacaoCurso/visualizacao_curso_controller.dart';
+import 'package:tcc_app/app/modules/cursos/modules/visualizacaoCurso/widgets/card_editavel_curso.dart';
+import 'package:tcc_app/app/modules/cursos/modules/visualizacaoCurso/widgets/card_informacoes_curso.dart';
 import 'package:tcc_app/models/Atividade.dart';
 import 'package:tcc_app/models/Curso.dart';
 import 'package:tcc_app/utils/formatacoes.dart';
-import 'package:tcc_app/widgets/card_overlap_title.dart';
-import 'package:tcc_app/widgets/change_editable_text_field.dart';
 import 'package:tcc_app/widgets/loading.dart';
 import 'package:tcc_app/widgets/refresh_list.dart';
 
@@ -15,6 +14,11 @@ typedef OnSelectDate = void Function(DateTime date);
 typedef OnFocusFunction = void Function();
 typedef OnChangeTextField = void Function();
 
+// Essa página tem que mostrar todas as informações relacionadas a um curso,
+// ou seja, tanto a sua listagem de atividades quanto as suas matérias e informações
+// Será implementado então uma lista lateral com a primeira e segunda views como:
+//    1 - Nome do curso e listagem de atividades (botão +)
+//    2 - Informações adicionais do curso e listagem de matérias (botão +)
 class VisualizacaoCursoView extends GetView<VisualizacaoCursoController> {
   Widget buildAtividade(BuildContext context, Atividade atividade) {
     final double width = MediaQuery.of(context).size.width - 100.0;
@@ -60,207 +64,6 @@ class VisualizacaoCursoView extends GetView<VisualizacaoCursoController> {
           ],
         ),
       ),
-    );
-  }
-
-  Widget buildDateSelector(
-      BuildContext context, String hintText, DateTime dateTime,
-      {int maxLines = 1, OnSelectDate? onSelectDate}) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 8.0,
-        right: 8.0,
-        top: 2.0,
-        bottom: 2.0,
-      ),
-      child: Column(
-        children: [
-          Text(
-            hintText,
-            style: const TextStyle(color: Colors.black),
-          ),
-          const SizedBox(
-            height: 5.0,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: InkWell(
-                  onTap: () async {
-                    DateTime? date = await showDatePicker(
-                        context: context,
-                        initialDate: dateTime,
-                        firstDate: DateTime(DateTime.now().year - 1),
-                        lastDate: DateTime(DateTime.now().year + 1));
-                    if (date != null && onSelectDate != null) {
-                      onSelectDate(date);
-                    }
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20.0),
-                    child: Container(
-                      color: Colors.black,
-                      child: Align(
-                        alignment: Alignment.topCenter,
-                        child: Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: Text(
-                            diaComAno(
-                              dateTime,
-                            ),
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildTextField(
-    TextEditingController textEditingController,
-    String hintText, {
-    int maxLines = 1,
-    OnFocusFunction? onFocusFunction,
-    TextInputType textInputType = TextInputType.text,
-    String? erroMessage,
-    OnChangeTextField? onChangeTextField,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 8.0,
-        right: 8.0,
-        top: 2.0,
-        bottom: 2.0,
-      ),
-      child: Column(
-        children: [
-          Text(
-            hintText,
-            style: const TextStyle(color: Colors.black),
-          ),
-          const SizedBox(
-            height: 5.0,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20.0),
-                  child: Container(
-                    color: Colors.black,
-                    child: Row(
-                      children: <Widget>[
-                        const SizedBox(width: 8.0),
-                        Expanded(
-                          child: TextField(
-                            keyboardType: textInputType,
-                            onTap: onFocusFunction,
-                            controller: textEditingController,
-                            decoration: InputDecoration(
-                              hintText: hintText,
-                              border: InputBorder.none,
-                              labelStyle: const TextStyle(color: Colors.white),
-                              hintStyle: const TextStyle(color: Colors.grey),
-                              // errorText: erroMessage,
-                            ),
-                            style: const TextStyle(color: Colors.white),
-                            maxLines: maxLines,
-                            onChanged: (value) {
-                              if (onChangeTextField != null) {
-                                onChangeTextField();
-                              }
-                            },
-                          ),
-                        ),
-                        const SizedBox(width: 8.0),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          erroMessage != null
-              ? Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 10.0, bottom: 2.0),
-                    child: Text(
-                      erroMessage,
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.red[300],
-                      ),
-                    ),
-                  ),
-                )
-              : Container(),
-        ],
-      ),
-    );
-  }
-
-  Widget duracaoCurso() {
-    final Curso curso = controller.curso;
-    int diasCurso = curso.fimCurso.difference(curso.inicioCurso).inDays;
-    int diasCursados = DateTime.now().difference(curso.inicioCurso).inDays;
-    if (diasCursados > diasCurso) {
-      diasCursados = diasCurso;
-    }
-
-    return CircularPercentIndicator(
-      radius: 70.0,
-      lineWidth: 8.0,
-      animation: true,
-      percent: 0.7,
-      center: Text(
-        diasCursados.toString() + "/" + diasCurso.toString(),
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
-        ),
-        textScaleFactor: 0.8,
-      ),
-      footer: const Padding(
-        padding: EdgeInsets.only(
-          top: 5.0,
-        ),
-        child: Text(
-          "Dias Curso",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 17.0, color: Colors.black),
-        ),
-      ),
-      circularStrokeCap: CircularStrokeCap.round,
-      progressColor: Colors.purple,
-    );
-  }
-
-  Widget buildCampo(String label, String value) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label + ":",
-          textAlign: TextAlign.start,
-          style:
-              const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          value,
-          style: const TextStyle(color: Colors.black),
-        ),
-      ],
     );
   }
 
@@ -370,184 +173,32 @@ class VisualizacaoCursoView extends GetView<VisualizacaoCursoController> {
           () => RefreshListView(
             header: Column(
               children: [
-                Stack(
-                  alignment: Alignment.topLeft,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Card(
-                          color: Colors.white,
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                              bottom: 5.0,
-                              left: 5.0,
-                              right: 5.0,
-                            ),
-                            child: controller.editandoCurso
-                                ? Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      buildTextField(
-                                        controller.nomeCurso,
-                                        "Nome",
-                                        erroMessage: controller.nomeCursoErro,
-                                        onChangeTextField:
-                                            controller.onChangeNomeCurso,
-                                      ),
-                                      buildTextField(
-                                        controller.descricaoCurso,
-                                        "Descrição",
-                                        erroMessage:
-                                            controller.descricaoCursoErro,
-                                        onChangeTextField:
-                                            controller.onChangeDescricaoCurso,
-                                      ),
-                                      buildDateSelector(
-                                        context,
-                                        "Início do curso",
-                                        controller.inicioCurso,
-                                        onSelectDate: (dateTime) {
-                                          controller.setInicioCurso(dateTime);
-                                        },
-                                      ),
-                                      buildDateSelector(
-                                        context,
-                                        "Fim do curso",
-                                        controller.fimCurso,
-                                        onSelectDate: (dateTime) {
-                                          controller.setFimCurso(dateTime);
-                                        },
-                                      ),
-                                    ],
-                                  )
-                                : Container(
-                                    width: MediaQuery.of(context).size.width,
-                                    padding: const EdgeInsets.only(top: 10.0),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        SizedBox(
-                                          width: MediaQuery.of(context)
-                                                  .size
-                                                  .width -
-                                              110.0,
-                                          child: Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              buildCampo(
-                                                "Nome",
-                                                controller.curso.nome,
-                                              ),
-                                              buildCampo(
-                                                "Descrição",
-                                                controller.curso.descricao,
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        duracaoCurso(),
-                                      ],
-                                    )),
-                          )),
-                    ),
-                    Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              "Curso",
-                              textScaleFactor: 1.2,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
                 controller.permissaoEditar
-                    ? Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: controller.editandoCurso
-                            ? [
-                                TextButton(
-                                  onPressed: () {
-                                    controller.sairModoEdicaoCurso();
-                                  },
-                                  child: const Text("Cancelar"),
-                                  style: TextButton.styleFrom(
-                                    padding: const EdgeInsets.all(10.0),
-                                    primary: Colors.white,
-                                    backgroundColor: Colors.red,
-                                    onSurface: Colors.grey,
-                                    textStyle: const TextStyle(fontSize: 17.0),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 10.0,
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    controller.salvarEdicaoCurso();
-                                  },
-                                  child: const Text("Salvar"),
-                                  style: TextButton.styleFrom(
-                                    padding: const EdgeInsets.all(10.0),
-                                    primary: Colors.white,
-                                    backgroundColor: Colors.green,
-                                    onSurface: Colors.grey,
-                                    textStyle: const TextStyle(fontSize: 17.0),
-                                  ),
-                                )
-                              ]
-                            : [
-                                TextButton(
-                                  onPressed: () {
-                                    controller.entrarModoEdicaoCurso();
-                                  },
-                                  child: const Text("Editar"),
-                                  style: TextButton.styleFrom(
-                                    padding: const EdgeInsets.all(10.0),
-                                    primary: Colors.white,
-                                    backgroundColor: Colors.teal,
-                                    onSurface: Colors.grey,
-                                    textStyle: const TextStyle(fontSize: 17.0),
-                                  ),
-                                )
-                              ],
+                    ? CardEditavelCurso(
+                        nomeCurso: controller.nomeCurso,
+                        descricaoCurso: controller.descricaoCurso,
+                        inicioCurso: controller.inicioCurso,
+                        fimCurso: controller.fimCurso,
+                        editando: controller.editandoCurso,
+                        nomeCursoErro: controller.nomeCursoErro,
+                        descricaoCursoErro: controller.descricaoCursoErro,
+                        onChangeNomeCurso: controller.onChangeNomeCurso,
+                        onChangeDesricaoCurso:
+                            controller.onChangeDescricaoCurso,
+                        onSelectDataInicial: controller.setInicioCurso,
+                        onSelectDataFinal: controller.setFimCurso,
+                        cancelarEdicao: controller.sairModoEdicaoCurso,
+                        salvarEdicao: controller.salvarEdicaoCurso,
+                        entrarModoEdicao: controller.entrarModoEdicaoCurso,
+                        permissaoEditar: controller.permissaoEditar,
+                        erro: controller.erro,
                       )
-                    : Container(),
-                controller.erro != null
-                    ? Text(
-                        controller.erro!,
-                        style: const TextStyle(
-                          color: Colors.red,
-                        ),
-                        textScaleFactor: 0.8,
-                      )
-                    : Container(),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                // const Text(
-                //   "Atividades",
-                //   textScaleFactor: 1.2,
-                // ),
+                    : CardInformacoesCurso(
+                        nomeCurso: controller.curso.nome,
+                        descricaoCurso: controller.curso.descricao,
+                        inicioCurso: controller.curso.inicioCurso,
+                        fimCurso: controller.curso.fimCurso,
+                      ),
                 cardRotuloAtividades(context),
                 const SizedBox(
                   height: 10.0,
